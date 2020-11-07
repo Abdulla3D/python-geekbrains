ae79ad75c4bf2c370cb2a477921eca4be3fe4eea
Abdulla3D <mail@abi-nur.com>
2020-11-07 22:29:30 +0600

    homework1_1

---
+++ Lessons1/homeTask3.py
@@ -0,0 +1,6 @@
+n = input("Enter an integer: ")
+while n < '0':
+    print("I have asked for number greater than 0! Please try again!")
+    n = input('Please enter number greater than 0: ')
+
+print(f"{n} + {n+n} + {n+n+n} = {int(n) + int(n+n) + int(n+n+n)}")
