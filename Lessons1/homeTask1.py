bd862acdc271691e7fc4f38930c9bef9ae265b36
Abdulla3D <mail@abi-nur.com>
2020-11-07 22:06:29 +0600

    homework1_1

--- Lessons1/homeTask1.py
+++ Lessons1/homeTask1.py
@@ -1,1 +1,8 @@
-"dsfsdfgdsfdsf"
+a = "hello"
+b = "GeekBrains!"
+print(f"{a}, {b}")
+numb1 = int(input("Enter any number: "))
+numb2 = int(input("Enter any number one more time: "))
+print(f"You have choosen the number {numb1} and {numb2}")
+word = input("Enter any word: ")
+print (f"{word} - it`s good choice")
